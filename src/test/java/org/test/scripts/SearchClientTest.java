e87026a65ec45093f00218ec25a6e2437f5e8266
QualityAssurance - Bohar Singh <Bohar.Singh@quadlabs.com>
2020-03-02 14:31:29 +0530

    code written for SearchClientTest.java

--- src/test/java/org/test/scripts/SearchClientTest.java
+++ src/test/java/org/test/scripts/SearchClientTest.java
@@ -1,5 +1,43 @@
 package org.test.scripts;
 
-public class SearchClientTest {
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.test.utilities.TestBase;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class SearchClientTest extends TestBase {
+	
+	@BeforeSuite
+	public void setup() throws IOException
+	{
+		init();
+	}
+	
+	@BeforeMethod
+	public void browserLaunch()
+	{
+		launchBrowser();
+		reports.createTest("Search Corporate");
+	}
+	
+	@Test
+	public void searchClient() throws Exception
+	{
+		 ArrayList<String> data = getRowData("TestData", 6);
+		   
+		    //System.out.println(corName);
+		    
+		    getWebElement("CompID").sendKeys(data.get(1));
+		    getWebElement("UserName").sendKeys(data.get(2));
+			getWebElement("pass").sendKeys(data.get(3));
+			getWebElement("loginBtn").click();
+			Thread.sleep(2000);
+			
+			 TestBase.frames("login");
+			 TestBase.frames("leftbar");
+	}
 
 }
